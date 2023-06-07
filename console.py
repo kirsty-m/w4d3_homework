646c6b4685eb63faaeb2f521e53a4a39a95bf62d
Kirsty <kirstmcbride@gmail.com>
2023-06-07 19:09:56 +0100

    author_repository done

--- console.py
+++ console.py
@@ -3,3 +3,6 @@ import pdb
 from models.author import Author
 from models.book import Book
 
+import repositories.author_repository as author_repository
+import repositories.book_repository as book_repository
+
---
+++ repositories/author_repository.py
@@ -0,0 +1,51 @@
+from db.run_sql import run_sql
+
+from models.author import Author
+from models.book import Book
+
+def select_all():
+    authors = []
+    sql = "SELECT * FROM authors"
+    results = run_sql(sql)
+
+    for row in results:
+        author = Author(row['name'], row['id'])
+        authors.append(author)
+    return author
+
+
+def select(id):
+    author = None
+    sql = "SELECT * FROM authors WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        author = Author(result['name'], result['id'])
+    return author
+
+
+def delete_all():
+    sql = "DELETE FROM authors"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM authors WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)
+
+
+def save(author):
+    sql = "INSERT INTO authors (name) VALUES (%s) RETURNING *"
+    values = [author.name]
+    result = run_sql(sql, values)
+    id = result[0]['id']
+    author.id
+    return author
+
+
+def update(author):
+    sql = "UPDATE authors SET (name) = %s WHERE id = %s"
+    values = [author.name, author.id]
+    run_sql(sql, values)
+
